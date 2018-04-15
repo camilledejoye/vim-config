2a07ce89e97ffe27f612e64367dab4738c96eacd
Camille Dejoye <camilledejoye@gmail.com>
2018-04-16 00:45:02 +0200

    Adds an install script to launch after cloning the repo

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [ -d 'pack' ]; then
+  echo 'Already installed'
+  exit
+fi
+
+cmd='vim'
+
+if hash nvim 2>/dev/null; then
+  cmd='nvim'
+elif ! hash lvim 2>/dev/null; then
+  echo 'You must intall vim or nvim first'
+  exit
+fi
+
+if [ ! -d 'pack/minpac/opt/minpac' ]; then
+  git clone https://github.com/k-takata/minpac.git pack/minpac/opt/minpac/
+fi
+
+$cmd -c MinpacUpdate
